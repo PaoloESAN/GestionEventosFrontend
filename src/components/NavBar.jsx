5a1c1e1432c0b59514a5b958ab9baf2d255e6ccb
Paolo <186249117+PaoloESAN@users.noreply.github.com>
2025-06-11 18:52:15 -0500

    tema por defecto al cerrar sesion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -65,7 +65,14 @@ function NavBar({ tabAct }) {
                             </li>
                             <div className="divider my-1"></div>
                             <li>
-                                <Link onClick={() => localStorage.removeItem('userId')} to="/login" className="py-2 px-3 rounded-md hover:bg-error hover:text-error-content">
+                                <Link
+                                    onClick={() => {
+                                        localStorage.removeItem('userId');
+                                        localStorage.removeItem('theme');
+                                        document.documentElement.setAttribute('data-theme', 'default');
+                                    }}
+                                    to="/login"
+                                    className="py-2 px-3 rounded-md hover:bg-error hover:text-error-content">
                                     <span className="flex items-center gap-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
